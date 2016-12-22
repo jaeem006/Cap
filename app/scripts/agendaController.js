9bfb52f443db6f3a8a99faa1934475e37e58146b
Javier Enríquez <javierem_94@ciencias.unam.mx>
2016-12-22 16:47:39 -0600

    Add files via upload

--- app/scripts/agendaController.js
+++ app/scripts/agendaController.js
@@ -1,10 +1,20 @@
 angular
 	.module('agendaApp')
-	.controller('agendaController', function($scope, conFactory) {
+	.controller('agendaController', function($scope, $http) {
 
-		var c = conFactory.getCon();
-		$scope.contactos = c;
-		$scope.nuevoContacto = {};
+		var c;
+
+
+		$http({
+        method : "GET",
+        url : "http://localhost:5000/get_contactos"
+    }).then(function mySucces(response) {
+        c = JSON.parse(response.data);
+				$scope.contactos = c;
+				console.log($scope.contactos);
+    }, function myError(response) {
+    	console.log("Hubo un error al conectar con la Base de datos")
+    });
 
 		var reCalle = /^\w/;
 		var reNum = /\d$/;
@@ -21,25 +31,38 @@ angular
 				reMovil.test(contacto.Movil) &&
 				reCalle.test(contacto.Calle) &&
 				reNum.test(contacto.Calle) &&
-				reColonia.test(contacto.Colonia));
+				reColonia.test(contacto.Colonia)); 
 
 			return v;
 		}
 		$scope.agregarContacto = function(newCotnact) {
-			if(verificar(newCotnact)){
-				$scope.contactos.push(newCotnact);
-				localStorage.setItem(newCotnact.Nombre , JSON.stringify(newCotnact));
-				$scope.nuevoContacto = {};
-				$scope.addContact = false;
-				console.log(JSON.stringify($scope.contactos));
-			}
+				$http({
+        method : "POST",
+        url : "http://localhost:5000/post_contacto/"+newCotnact.nombre+"/"+newCotnact.telefono+"/"+newCotnact.movil+"/"+newCotnact.calle+"/"+newCotnact.colonia+"/"+newCotnact.cp+""
+   			}).then(function mySucces(response) {
+   				console.log("agregado correctamente")
+   				$scope.contactos.push(newCotnact);
+   				$scope.nuevoContacto = {};
+					$scope.addContact = false;
+					console.log(JSON.stringify($scope.contactos));
+    		}, function myError(response) {
+    			console.log("Hubo un error al agregar a la Base de datos")
+    		});
 		}
 
 		$scope.deleteCon = function(contacto) {
-			var index = $scope.contactos.indexOf(contacto);
-			$scope.contactos.splice(index, 1);
-			localStorage.removeItem(contacto.Nombre);
-			console.log(JSON.stringify($scope.contactos));
+
+			$http({
+				method: "DELETE",
+				url :"http://localhost:5000/delete_contacto/"+ contacto.id + ""
+			}).then(function mySucces(response){
+					var index = $scope.contactos.indexOf(contacto);
+					$scope.contactos.splice(index, 1);
+					console.log(JSON.stringify($scope.contactos));
+			}, function myError(response) {
+				console.log("Hubo un error al eliminar de la Base de datos");
+			});
+
 		}
 
 		$scope.editContacto =function(contacto) {
@@ -49,11 +72,9 @@ angular
 		}
 
 		$scope.guardarContacto = function(contacto) {
-			if (verificar(contacto)){
 				$scope.editContact = false;
 				$scope.existingContacto = {};
 				$scope.agregarContacto(contacto);
-			}
 		}
 		
 	});
